b900889c37841e19e5d7d157d159767361d115eb
https://github.com/Jack-tech32 <jp0506129@gmail.com>
2025-12-30 18:56:56 +0530

    Fix runtime + stable dependencies

--- scripts/predict_xray.py
+++ scripts/predict_xray.py
@@ -2,20 +2,22 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 
-# Load saved CNN model
 model = tf.keras.models.load_model("../models/pneumonia_cnn_final.h5")
 
+IMG_SIZE = 224   # MUST MATCH TRAINING
+
 def predict_xray(img_path):
-    img = image.load_img(img_path, target_size=(64, 64))
-    img = image.img_to_array(img)/255.0
+    img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
+    img = image.img_to_array(img)
+    img = img / 255.0
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img)[0][0]
 
-    if pred > 0.5:
-        print("PNEUMONIA DETECTED")
+    if pred >= 0.5:
+        print("🦠 PNEUMONIA DETECTED")
     else:
-        print("NORMAL LUNGS")
+        print("🫁 NORMAL LUNGS")
 
-# Example test
-predict_xray(r"D:\final_project\data\chest_xray\chest_xray\test\PNEUMONIA\person1_virus_6.jpeg")
+if __name__ == "__main__":
+    predict_xray(r"D:\final_project\data\chest_xray\chest_xray\test\NORMAL\IM-0001-0001.jpeg")
